676fcdd4bac5dacf7fc8b011102103c19012c96b
fedya <1bbsk1@users.noreply.github.com>
2025-03-07 02:04:54 +0300

    refactor: resolve merge conflicts and update build configuration
    - Add TypeScript support in webpack and babel configs
    
    - Standardize CSS loader configuration to use MiniCssExtractPlugin
    
    - Fix HTML templates to properly include webpack assets
    
    - Update package.json with TypeScript dependencies and scripts

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,26 @@
-module.exports = {
-  presets: [
-    ["@babel/preset-env", { useBuiltIns: "usage", corejs: "3.0.0" }],
-    "@babel/preset-typescript",
+const presets = [
+  [
+    "@babel/preset-env",
+    {
+      targets: {
+        edge: "17",
+        firefox: "60",
+        chrome: "64",
+        safari: "11.1",
+        esmodules: true,
+        ie: "11",
+      },
+      useBuiltIns: "usage",
+      corejs: "3.36.0",
+    },
   ],
-  plugins: ["@babel/plugin-transform-class-properties"],
-};
+  "@babel/preset-typescript",
+];
+
+const plugins = [
+  ["@babel/plugin-transform-class-properties", { loose: true }],
+  ["@babel/plugin-transform-private-methods", { loose: true }],
+  ["@babel/plugin-transform-private-property-in-object", { loose: true }],
+];
+
+module.exports = { presets, plugins };
